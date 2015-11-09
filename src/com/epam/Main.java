d8fb4151d43a4c24c6780171d536079c736121a6
Maryia Ihnatsenka <maryia_ihnatsenka@epam.com>
2015-11-09 15:09:46 +0300

    v1.2

--- src/com/epam/Main.java
+++ src/com/epam/Main.java
@@ -1,92 +1,85 @@
 package com.epam;
+
 import java.util.Scanner;
 import java.io.IOException;
 import java.util.InputMismatchException;
+
 import com.epam.Operations;
 
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static String item;
+    static double num1 = 0, num2, num3 = 0;
 
-    public static void main(String[] args) throws IOException  {
+    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
-try{
-        Operations result = new Operations();
 
-        String item;
-        boolean cont=true;
-        while(cont){
-        double num1, num2, num3;
-        String operation;
+        try {
+            Operations result = new Operations();
+            boolean cont = true;
+            while (cont) {
 
-    System.out.println("Enter num");
-    num1 = sc.nextDouble();
 
-    System.out.println("Enter your operation");
-    operation = sc.next();
+                String operation;
+                if (num1 == 0) {
+                    System.out.println("Enter num");
+                    num1 = sc.nextDouble();
+                }
+                System.out.println("Enter your operation");
+                operation = sc.next();
 
-    System.out.println("Enter one more num");
-    num2 = sc.nextDouble();
+                System.out.println("Enter one more num");
+                num2 = sc.nextDouble();
+                switch (operation) {
+                    case "+":
+                        num3 = result.add(num1, num2);
+                        System.out.println(num3);
+                        break;
 
-            switch (operation) {
-                case "+":
-                    num3 = result.add(num1, num2);
-                    System.out.println(num3  + "Do you want to proceed? y/n");
-                    item = sc.next();
-                    if (item.equals("y")){
-                        cont=true;
-                    }else if (item.equals("n")) {
-                        cont = false;
-                        System.out.println("Exit");
-                    }
-                    break;
+                    case "-":
+                        num3 = result.substract(num1, num2);
+                        System.out.println(num3);
+                        break;
 
-                case "-":
-                    num3 = result.substract(num1, num2);
-                    System.out.println(num3  + "Do you want to proceed? y/n");
-                    item = sc.next();
-                    if (item.equals("y")){
-                        cont=true;
-                    }else if (item.equals("n")) {
-                        cont = false;
-                        System.out.println("Exit");
-                    }
-                    break;
+                    case "*":
+                        num3 = result.multiply(num1, num2);
+                        System.out.println(num3);
+                        break;
 
-                case "*":
-                    num3 = result.multiply(num1, num2);
-                    System.out.println(num3  + "Do you want to proceed? y/n");
-                    item = sc.next();
-                    if (item.equals("y")){
-                        cont=true;
-                    }else if (item.equals("n")) {
-                        cont = false;
-                        System.out.println("Exit");
-                    }
-                    break;
+                    case "/":
+                        try{
+                            num3 = result.divide(num1, num2);}
+                        catch(ArithmeticException e){
+                            System.out.println(e);
+                             Main.main(args);
+                        }
+                        System.out.println(num3);
+                        break;
 
-                case "/":
-                    if (num2==0)
-                    {
-                        System.out.println("Divide by zero is prohibited");}
-                    else{
-                    num3 = result.divide(num1, num2);
-                    System.out.println(num3  + "Do you want to proceed? y/n");
-                    item = sc.next();
-                    if (item.equals("y")){
-                        cont=true;
-                    }else if (item.equals("n")) {
-                        cont = false;
-                        System.out.println("Exit");
-                    }}
-                    break;
+                    default:
+                        System.out.println("Incorrect input");
+                }
+                System.out.println("Do you want to proceed with the last result (y) or start from the beginning(n) or exit(0)? ");
+                item = sc.next();
+                if (item.equals("y")) {
+                    num1 = num3;
+                    Main.main(args);
+                } else if (item.equals("n")) {
+                    num1 = 0;
+                } else if (item.equals("0")) {
+                    cont=false;
+                    System.out.println("Exit");
+                    sc.close();
+                }
             }
-    }
-    }catch (InputMismatchException e){
-    System.out.println("Incorrect input.Change input");
-    Main.main(args);
-}
 
-    }}
+        } catch (InputMismatchException e) {
+            System.out.println("Incorrect input.Change input");
+
+        }
+
+    }
+}
 
 
 
--- src/com/epam/Operations.java
+++ src/com/epam/Operations.java
@@ -1,5 +1,7 @@
 package com.epam;
 import com.epam.Main;
+
+
 /**
  * Created by Maryia_Ihnatsenka on 2015-10-27.
  */
@@ -7,27 +9,31 @@ import com.epam.Main;
 public class Operations {
 
     double add(double num1, double num2) {
-        double result = num1+num2;
+        double result = num1 + num2;
         return result;
     }
 
 
     double substract(double num1, double num2) {
-        double result = num1-num2;
+        double result = num1 - num2;
         return result;
     }
 
     double multiply(double num1, double num2) {
-        double result = num1*num2;
+        double result = num1 * num2;
         return result;
     }
 
     double divide(double num1, double num2) {
-        double result = num1/num2;
+        if(num2==0) {
+            throw new ArithmeticException ("Divide by zero is prohibited");
+        }
+           double result = num1 / num2;
         return result;
     }
 
-
 }
+
+
 
 
